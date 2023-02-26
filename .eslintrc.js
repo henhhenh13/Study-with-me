61c55a07258ea35747d32b8858eaab5ddd109666
Hoàng Hênh <74145131+henhhenh13@users.noreply.github.com>
2023-02-26 16:45:34 +0700

    chore: config eslint and lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: 'standard-with-typescript',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
+    project: './tsconfig.json'
   }
 }
