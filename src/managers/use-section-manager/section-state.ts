708b9f21492aac481d0decff80bc905d4c6769a1
Hoàng Hênh <74145131+henhhenh13@users.noreply.github.com>
2023-03-07 22:27:48 +0700

    feat: add section manager

--- src/managers/use-section-manager/section-state.ts
+++ src/managers/use-section-manager/section-state.ts
@@ -1,1 +1,8 @@
+import { atom } from 'recoil';
+
 export type SectionState = 'units' | 'vocabularys' | 'exercise';
+
+export const SECTION_STATE = atom<SectionState>({
+  key: 'sectionState',
+  default: 'units',
+});
--- src/sections/sidebar.tsx
+++ src/sections/sidebar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { useSectionManager } from '../managers/use-section-manager/use-section-manager';
+
 export const Sidebar = (): React.ReactElement => {
+  const { updateSection } = useSectionManager();
   return (
     <div className="w-[15%] shadow-lg border-r h-full bg-white">
       <h1 className="py-8 font-bold text-blue-500 text-xl text-center">
@@ -8,10 +11,16 @@ export const Sidebar = (): React.ReactElement => {
       </h1>
       <div className="w-full">
         <ul className="divide-y px-8 font-semibold">
-          <li className="py-2 cursor-pointer hover:text-blue-500 transition-colors duration-200">
+          <li
+            className="py-2 cursor-pointer hover:text-blue-500 transition-colors duration-200"
+            onClick={() => updateSection('units')}
+          >
             Units
           </li>
-          <li className="py-2 cursor-pointer hover:text-blue-500 transition-colors duration-200">
+          <li
+            className="py-2 cursor-pointer hover:text-blue-500 transition-colors duration-200"
+            onClick={() => updateSection('vocabularys')}
+          >
             Vocabularys
           </li>
         </ul>
