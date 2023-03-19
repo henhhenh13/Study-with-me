ebc53db7391a1fe331af58e477f72653aac0c7eb
Hoàng Hênh <74145131+henhhenh13@users.noreply.github.com>
2023-03-19 22:18:20 +0700

    refactor: active exercise

--- src/managers/active-exercise/active-exercise-state.ts
+++ src/managers/active-exercise/active-exercise-state.ts
@@ -1,28 +1,19 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
-import { ExerciseSerialized } from '../exercise/exercise-serialized';
-import { EXERCISE_STATE } from '../exercise/exercise-state';
-import { SentenseState } from '../sentense/sentense-state';
-import { SerializedVocabulary } from '../vocabularies/interface';
+import { Exercise } from '../exercise/interface';
 
-export interface ActiveExerciseState extends ExerciseSerialized {
-  vocabularies?: SerializedVocabulary[];
-  sentenses?: SentenseState[];
-}
-
-export const ACTIVE_EXERCISE_ID_STATE = atom<string | undefined>({
-  key: 'activeExerciseIndex',
-  default: undefined,
-});
-
-export const ACTIVE_EXERCISE_STATE = selector<ActiveExerciseState | undefined>({
-  key: 'activeExercise',
-  get: ({ get }) => {
-    const exercises = get(EXERCISE_STATE);
-    const activeExerciseId = get(ACTIVE_EXERCISE_ID_STATE);
-
-    if (activeExerciseId && exercises) {
-      return exercises.get(activeExerciseId);
-    } else return undefined;
+const initialActiveExercise: Exercise = {
+  exerciseId: 'exerciseId',
+  title: 'Title',
+  exerciseType: 'vocabulary',
+  vocabularies: [],
+  flags: {
+    isVocabularyExercise: false,
+    hasVocabularyExercise: false,
   },
+};
+
+export const ACTIVE_EXERCISE = atom<Exercise>({
+  key: 'activeExercise',
+  default: initialActiveExercise,
 });
