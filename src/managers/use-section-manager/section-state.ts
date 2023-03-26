1c63c18adee8af0dae1b9294b9713e626b318305
Hoàng Hênh <74145131+henhhenh13@users.noreply.github.com>
2023-03-26 20:41:05 +0700

    fix: rollback section

--- src/managers/use-section-manager/section-state.ts
+++ src/managers/use-section-manager/section-state.ts
@@ -4,5 +4,5 @@ export type SectionState = 'units' | 'vocabularies' | 'exercise' | 'grammars';
 
 export const SECTION_STATE = atom<SectionState>({
   key: 'sectionState',
-  default: 'grammars',
+  default: 'units',
 });
